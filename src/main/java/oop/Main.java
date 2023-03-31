b96c633f57058f88d08280735702f0436d28eee3
zmazan12g <zmazan12g@gmail.com>
2023-03-31 15:52:06 -0400

    Finalisation

--- src/main/java/oop/Main.java
+++ src/main/java/oop/Main.java
@@ -8,11 +8,9 @@ public class Main {
     public static void main(String[] args) {
         HourlyEmployee hourler = new HourlyEmployee("Jim", 15, 40);
         hourler.printBadge();
-        hourler.printPay();
 
         SalariedEmployee salarier = new SalariedEmployee("Mike", 104_285.71);
         salarier.printBadge();
-        salarier.printPay();
 
         Entrepreneur entreprenerier = new Entrepreneur("Rob", 2000, 500);
         System.out.println(entreprenerier);
